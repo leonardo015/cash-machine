08dfd40d126630f262b569f2709742af8bd627e5
Leonardo Sampaio <leonardo.html@gmail.com>
2018-04-21 03:03:27 -0300

    clearing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import CashMachine from './CashMachineContainer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-it('changes CashMachine.requestedAmount', () => {
-  // const obj = new CashMachine();
-  // obj.changeRequestedAmount(10);
-  // console.log(obj.state);
 });
